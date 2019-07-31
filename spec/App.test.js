799bccfc7c302dedae623bb6f4e8a48a2d864743
Gregory <gregoryinouye@gmail.com>
2019-07-31 17:44:41 -0500

    edit jest setup

--- spec/App.test.js
+++ spec/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import App from './App';
+import App from '../client/src/App';
 
 describe('App Component Tests', () => {
-  it('<App /> renders without crashing', () => {
+  it('<App /> renders', () => {
     shallow(<App />);
   });
 });
